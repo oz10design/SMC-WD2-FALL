f747df9ef0b2fe9db84cf3d6b176736dad0d629d
Oscar <Montiel_Licona_Oscar@student.smc.edu>
2021-12-14 17:55:38 -0800

    Finished Site1

--- Final-project-structure/main.js
+++ Final-project-structure/main.js
@@ -1,6 +1,15 @@
-const readMoreBtn = document.querySelector('.read-more-btn');
-const text = document.querySelector('.text');
-
-readMoreBtn.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
-})
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
